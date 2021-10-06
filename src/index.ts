e1b7371973e4cff1f0e374439797084f5bd6c753
dpeter99 <dpeter99@gmail.com>
2021-10-06 23:30:42 +0200

    index.ts clean up

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 import {Archivist, Config} from "./Archivist.ts";
 
-import * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
-
 export function run(config:any) {
-    /*
-    const confFile = Deno.cwd() + "/archivist.config.ts";
-    if(!fs.existsSync(confFile)){
-        console.error("There is no config file at: '" + confFile );
-    }
-
-    let confmodule = await import("file://"+confFile);
-
-    if(confmodule.config == undefined){
-        console.error("The given config file does not export a 'config'");
-    }
-
-    const conf: Config = confmodule.config;
-    */
 
     const arch: Archivist = new Archivist(config);
 
@@ -25,7 +9,7 @@ export function run(config:any) {
 }
 
 
-export let version = "0.0.0";
+export let version = "0.1.0-alpha01";
 
 export * from "./Archivist.ts";
 export * from "./Pipeline.ts";
